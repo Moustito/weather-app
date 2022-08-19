dd6ae90ca47c6a68ce21359d525466922d1e2892
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-08-19 16:22:04 +0200

    js- finish menu autocomplete, finish background random of city, start forcast footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,38 +8,45 @@ jours = new Array(
   "Vendredi",
   "Samedi"
 );
+let tomorow = date.getDay()+7
 
-async function fetchQuotes() {
+let clickText = "";
+
+async function displayWheater() {
+  console.log(clickText);
   try {
-    //  const response = await fetch("http://api.openweathermap.org/data/2.5/forecast?q=vancouver&units=metric&appid=80131affd762f69e36c099cae83e779f");
-    //  const data = await response.json();
-    // console.log(data.city.name);
-    // console.log(data.list[0].dt_txt);
-    // console.log(data.list[0].main.temp + "°c");
-    // console.log(data.list[0].weather[0].main);
-    // console.log(data.list[0].main.temp_max +"/"+ data.list[0].main.temp_min + "°C");
-    //  console.log(data);
+    const response = await fetch(
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+        clickText +
+        "&units=metric&appid=80131affd762f69e36c099cae83e779f"
+    );
+    const data = await response.json();
+
     // MAIN
-    // document.getElementById("town").innerHTML = data.city.name;
-    // document.getElementById("date").innerHTML = data.list[0].dt_txt;
-    // document.getElementById("degree").innerHTML = data.list[0].main.temp + "°c";
-    // document.getElementById("wheater").innerHTML = data.list[0].weather[0].description;
-    // document.getElementById("min-max").innerHTML = data.list[0].main.temp_max +"/"+ data.list[0].main.temp_min + "°C";
-    // let iconWeather = document.getElementById("icon")
-    // iconWeather.src = "assets/images/icon/"+data.list[0].weather[0].main+".png"
-    // document.getElementById("icon").alt = data.list[0].weather[0].main;
+    document.getElementById("town").innerHTML = data.city.name;
+    document.getElementById("date").innerHTML = data.list[0].dt_txt;
+    document.getElementById("degree").innerHTML = data.list[0].main.temp + "°c";
+    document.getElementById("wheater").innerHTML =
+      data.list[0].weather[0].description;
+    document.getElementById("min-max").innerHTML =
+      data.list[0].main.temp_max + "/" + data.list[0].main.temp_min + "°C";
+    let iconWeather = document.getElementById("icon");
+    iconWeather.src =
+      "assets/images/icon/" + data.list[0].weather[0].main + ".png";
+    document.getElementById("icon").alt = data.list[0].weather[0].main;
+
     // FOOTER
-    // for (let i=0 ; i < 3; i++) {
-    // let forcast__img = document.getElementById("forcast__img"+[i])
-    // forcast__img.src = "assets/images/icon/"+data.list[0].weather[0].main+".png"
-    // document.getElementById("forcast__img"+[i]).alt = data.list[0].weather[0].main;
-    // document.getElementById("forcast__minMax"+[i]).innerHTML = data.list[0].main.temp_max +"/"+ data.list[0].main.temp_min + "°C";
-    // document.getElementById("forcast__day"+[i]).innerHTML = jours[x+i+1];
+    for (let i = 0; i < 3; i++) {
+      let forcast__img = document.getElementById("forcast__img" + i);
+      forcast__img.src = `assets/images/icon/${data.list[i].weather[0].main}.png`;
+      document.getElementById("forcast__img" + [i]).alt =data.list[i].weather[0].main;
+      document.getElementById("forcast__minMax"+i).innerHTML =data.list[i].main.temp_max + "/" + data.list[i].main.temp_min + "°C";
+      document.getElementById("forcast__day"+i).innerHTML = jours[tomorow + i];
+    }
   } catch (error) {
-    alert("Error", error);
+    console.log(error);
   }
 }
-fetchQuotes();
 
 function displayForcastWheater() {
   for (let i = 0; i < 3; i++) {
@@ -67,7 +74,21 @@ function displayForcastWheater() {
     footer__div__day.id = "forcast__day" + [i];
   }
 }
+
 // displayForcastWheater()
+
+let dataArray = [];
+
+searchLocation.addEventListener("keyup", (event) => {
+  dataArray = [];
+  fetchQuotes();
+  removeMenuAutocomplete();
+  if (event.target.value == "") {
+    document.getElementById("divCountry").style.display = "none";
+  } else {
+    document.getElementById("divCountry").style.display = "block";
+  }
+});
 
 async function fetchQuotes() {
   try {
@@ -75,33 +96,54 @@ async function fetchQuotes() {
       "https://api.teleport.org/api/cities/?search=" + searchLocation.value
     );
     const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    alert("Error", error);
-  }
+    for (let i = 0; i < 3; i++) {
+      dataArray.push(
+        data._embedded["city:search-results"][i].matching_full_name
+      );
+    }
+    displayMenuAutocomplete();
+  } catch (error) {}
 }
-
-searchLocation.addEventListener("keyup", (event) => {
-  fetchQuotes();
-  if (!(document.getElementById("searchLocation") == "")) {
-    document.getElementById("divCountry").style.display = "block";
-  } else if (document.getElementById("searchLocation") == "") {
-    document.getElementById("divCountry").style.display = "none";
-  }
-});
 
 function displayMenuAutocomplete() {
-  let searchBar = document.getElementById("searchBar");
-  let divCountry = document.createElement("div");
-  searchBar.appendChild(divCountry);
-  divCountry.id = "divCountry";
-
-  let countryList = document.createElement("ul");
-  divCountry.appendChild(countryList);
-
-  let country = document.createElement("li");
-  countryList.appendChild(country);
-  let countryText = document.createTextNode("element.name");
-  country.appendChild(countryText);
+  let countryList = document.getElementById("countryList");
+  for (element of dataArray) {
+    let country = document.createElement("li");
+    countryList.appendChild(country);
+    let countryText = document.createTextNode(element);
+    country.appendChild(countryText);
+    country.id = "country";
+  }
 }
-displayMenuAutocomplete();
+
+function removeMenuAutocomplete() {
+  let country = document.querySelectorAll("li");
+  for (element of country) {
+    element.remove();
+  }
+}
+
+document.getElementById("countryList").addEventListener("click", (event) => {
+  const words = event.target.textContent.split(" ");
+  clickText = words[0];
+  document.getElementById("divCountry").style.display = "none";
+
+  displayWheater();
+  displayphoto();
+  searchLocation.value = "";
+});
+
+// https://api.unsplash.com/photos/random?query="+ clickText +"&client_id=MQo0FbNdtKznD1F3GbdvBQ5NbxCXu2_UaGYzuiPdNaI
+
+
+async function displayphoto() {
+  console.log(clickText);
+  try {
+    const response = await fetch("https://api.unsplash.com/photos/random?query="+clickText+"-city"+"&client_id=MQo0FbNdtKznD1F3GbdvBQ5NbxCXu2_UaGYzuiPdNaI");
+    const data = await response.json();
+    console.log(data.urls.raw);
+    document.body.style.backgroundImage = "url('"+data.urls.raw+"')";
+  } catch (error) {
+    console.log(error);
+  }
+}
